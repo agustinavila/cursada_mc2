019c224ff2597ed5da73af43afaacd02c9460f7c
agustinavila <tinto.avila@gmail.com>
2023-04-26 18:29:07 -0300

    Minor change in main

--- src/main.c
+++ src/main.c
@@ -1,13 +1,12 @@
-/*
- * Copyright 2022 NXP
- * NXP confidential.
- * This software is owned or controlled by NXP and may only be used strictly
- * in accordance with the applicable license terms.  By expressly accepting
- * such terms or by downloading, installing, activating and/or otherwise using
- * the software, you are agreeing that you have read, and that you agree to
- * comply with and are bound by, such license terms.  If you do not agree to
- * be bound by the applicable license terms, then you may not retain, install,
- * activate or otherwise use the software.
+/**
+ * @file main.c
+ * @author agustinavila (tinto.avila@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
  */
 
 #if defined (__USE_LPCOPEN)
