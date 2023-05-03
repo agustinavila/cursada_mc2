84ff26f7e2a967122e06896a74f20f58ee157480
agustinavila <tinto.avila@gmail.com>
2023-05-02 22:29:36 -0300

    Add buzzer to avoid floating value

---
+++ src/Driver/buzzer.h
@@ -0,0 +1,21 @@
+/**
+ * @file buzzer.h
+ * @author agustinavila (tinto.avila@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#if !defined(DRIVER_BUZZER_H)
+#define DRIVER_BUZZER_H
+
+#include "chip.h"
+
+void buzzer_init();
+void buzzer_prende();
+void buzzer_apaga();
+
+#endif // DRIVER_BUZZER_H
