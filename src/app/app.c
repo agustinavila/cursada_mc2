b6880d1628d3cf643dd3c9735e6eea6ffc35c6be
Agustin Avila <avila.juan.agustin@gmail.com>
2026-03-30 20:27:02 -0300

    Achata el flujo principal de la aplicacion

--- src/app/app.c
+++ src/app/app.c
@@ -17,8 +17,6 @@
 #include "control/control_on_off.h"
 #include "hmi/hmi.h"
 
-static control_on_off_configuracion_t app_control_on_off_configuracion_actual_;
-
 typedef struct {
     ds18b20_driver_t sensor_temperatura;
     bool inicializado;
@@ -68,25 +66,16 @@ static bool app_obtener_temperatura_sensor_principal(int16_t* temperatura_deci_c
     return true;
 }
 
-static void app_cargar_sensor_en_hmi(void)
+static void app_actualizar_sensores(void)
 {
     int16_t temperatura_deci_celsius = 0;
 
-    if (app_obtener_temperatura_sensor_principal(&temperatura_deci_celsius)) {
-        hmi_cargar_estado_sensor(true, temperatura_deci_celsius);
-    } else {
-        hmi_cargar_estado_sensor(false, 0);
-    }
-}
-
-static void app_actualizar_sensores(void)
-{
     if (!app_sensores_.inicializado) {
-        app_cargar_sensor_en_hmi();
+        hmi_cargar_estado_sensor(false, 0);
         return;
     }
 
-    ds18b20_process(&app_sensores_.sensor_temperatura, 20U);
+    ds18b20_process(&app_sensores_.sensor_temperatura, APP_LOOP_DELTA_MS);
 
     if (!ds18b20_is_busy(&app_sensores_.sensor_temperatura)) {
         app_sensores_.ticks_actualizacion++;
@@ -98,7 +87,11 @@ static void app_actualizar_sensores(void)
         app_sensores_.ticks_actualizacion = 0U;
     }
 
-    app_cargar_sensor_en_hmi();
+    if (app_obtener_temperatura_sensor_principal(&temperatura_deci_celsius)) {
+        hmi_cargar_estado_sensor(true, temperatura_deci_celsius);
+    } else {
+        hmi_cargar_estado_sensor(false, 0);
+    }
 }
 
 static void app_cargar_parametros_en_hmi(void)
@@ -112,10 +105,13 @@ static void app_cargar_parametros_en_hmi(void)
                                   parametros->control.modo_calentar);
 }
 
-static control_on_off_configuracion_t app_obtener_configuracion_control_desde_parametros(void)
+static void app_actualizar_control(void)
 {
+    const hmi_parametros_control_t parametros_hmi = hmi_obtener_parametros_control();
     const parametros_t* parametros = parametros_obtener();
-    control_on_off_configuracion_t configuracion = {
+    int16_t temperatura_deci_celsius = 0;
+    bool salida_activa = false;
+    control_on_off_configuracion_t configuracion_control = {
         .sentido = parametros->control.modo_calentar
             ? CONTROL_ON_OFF_SENTIDO_CALENTAR
             : CONTROL_ON_OFF_SENTIDO_ENFRIAR,
@@ -125,47 +121,23 @@ static control_on_off_configuracion_t app_obtener_configuracion_control_desde_pa
         .tiempo_minimo_apagado_ms = parametros->control.tiempo_minimo_apagado_ms,
     };
 
-    return configuracion;
-}
-
-static void app_sincronizar_control_desde_parametros(void)
-{
-    control_on_off_configuracion_t nueva_configuracion = app_obtener_configuracion_control_desde_parametros();
-
-    if ((nueva_configuracion.setpoint_deci_celsius == app_control_on_off_configuracion_actual_.setpoint_deci_celsius)
-        && (nueva_configuracion.histeresis_deci_celsius == app_control_on_off_configuracion_actual_.histeresis_deci_celsius)
-        && (nueva_configuracion.sentido == app_control_on_off_configuracion_actual_.sentido)
-        && (nueva_configuracion.tiempo_minimo_encendido_ms
-            == app_control_on_off_configuracion_actual_.tiempo_minimo_encendido_ms)
-        && (nueva_configuracion.tiempo_minimo_apagado_ms
-            == app_control_on_off_configuracion_actual_.tiempo_minimo_apagado_ms)) {
-        return;
-    }
-
-    app_control_on_off_configuracion_actual_ = nueva_configuracion;
-    control_on_off_configurar(app_control_on_off_configuracion_actual_);
-}
-
-static void app_sincronizar_hmi_en_parametros(void)
-{
-    const hmi_parametros_control_t parametros_hmi = hmi_obtener_parametros_control();
-
     if (parametros_actualizar_control(parametros_hmi.setpoint_deci_celsius,
                                       parametros_hmi.histeresis_deci_celsius,
                                       parametros_hmi.tiempo_minimo_encendido_ms,
                                       parametros_hmi.tiempo_minimo_apagado_ms,
                                       parametros_hmi.modo_calentar)) {
         (void) parametros_guardar();
+        parametros = parametros_obtener();
+        configuracion_control.sentido = parametros->control.modo_calentar
+            ? CONTROL_ON_OFF_SENTIDO_CALENTAR
+            : CONTROL_ON_OFF_SENTIDO_ENFRIAR;
+        configuracion_control.setpoint_deci_celsius = parametros->control.setpoint_deci_celsius;
+        configuracion_control.histeresis_deci_celsius = parametros->control.histeresis_deci_celsius;
+        configuracion_control.tiempo_minimo_encendido_ms = parametros->control.tiempo_minimo_encendido_ms;
+        configuracion_control.tiempo_minimo_apagado_ms = parametros->control.tiempo_minimo_apagado_ms;
     }
-}
 
-static void app_actualizar_control(void)
-{
-    int16_t temperatura_deci_celsius = 0;
-    bool salida_activa = false;
-
-    app_sincronizar_hmi_en_parametros();
-    app_sincronizar_control_desde_parametros();
+    control_on_off_configurar(configuracion_control);
 
     if (!app_obtener_temperatura_sensor_principal(&temperatura_deci_celsius)) {
         hmi_cargar_estado_control(false, false);
@@ -201,12 +173,24 @@ void app_init(void)
         (void) ds18b20_start_conversion(&app_sensores_.sensor_temperatura);
     }
 
-    app_cargar_sensor_en_hmi();
+    hmi_cargar_estado_sensor(false, 0);
     hmi_init();
     app_cargar_parametros_en_hmi();
 
-    app_control_on_off_configuracion_actual_ = app_obtener_configuracion_control_desde_parametros();
-    control_on_off_inicializar(app_control_on_off_configuracion_actual_);
+    {
+        const parametros_t* parametros = parametros_obtener();
+        control_on_off_configuracion_t configuracion_control = {
+            .sentido = parametros->control.modo_calentar
+                ? CONTROL_ON_OFF_SENTIDO_CALENTAR
+                : CONTROL_ON_OFF_SENTIDO_ENFRIAR,
+            .setpoint_deci_celsius = parametros->control.setpoint_deci_celsius,
+            .histeresis_deci_celsius = parametros->control.histeresis_deci_celsius,
+            .tiempo_minimo_encendido_ms = parametros->control.tiempo_minimo_encendido_ms,
+            .tiempo_minimo_apagado_ms = parametros->control.tiempo_minimo_apagado_ms,
+        };
+
+        control_on_off_inicializar(configuracion_control);
+    }
     app_ultimo_tick_procesado_ms_ = board_timer_get_ticks();
 }
 
